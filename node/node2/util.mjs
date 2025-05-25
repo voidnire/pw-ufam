3c437c6e428eb443cfb3165515e2afa44dcadaca
voidnire <nirerin15@gmail.com>
2025-05-25 16:07:45 -0400

    node2

---
+++ node/node2/util.mjs
@@ -0,0 +1,7 @@
+export function createLink(filename) {
+  return `<a href="/${filename}">${filename}</a><br>\n`;
+}
+
+export function createBackLink() {
+  return '<br><a href="/"> <h3>Voltar </h3></a>';
+}
